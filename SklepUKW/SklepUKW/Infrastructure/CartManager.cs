736403f4c2cbbf3a5434d5ffae8362384ecbb9a4
Adrianna Piszcz <adrianna.piszcz@vp.pl>
2021-04-25 19:28:33 +0200

    Mechanizm koszyka, sesje

---
+++ SklepUKW/SklepUKW/Infrastructure/CartManager.cs
@@ -0,0 +1,98 @@
+﻿using SklepUKW.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SklepUKW.Infrastructure
+{
+    public class CartManager
+    {
+        FilmsContext db;
+        ISessionManager session;
+
+        public CartManager(FilmsContext db, ISessionManager session)
+        {
+            this.db = db;
+            this.session = session;
+        }
+
+        public List<CartItem> GetItems()
+        {
+            List<CartItem> items;
+
+            if(session.Get<List<CartItem>>(Consts.CartSessionKey) == null)
+            {
+                items = new List<CartItem>();
+            }
+            else
+            {
+                items = session.Get<List<CartItem>>(Consts.CartSessionKey);
+            }
+
+            return items;
+        }
+
+        public void AddToCart(int filmId)
+        {
+            var cart = GetItems();
+            var thisFilm = cart.Find(f => f.Film.FilmId == filmId);
+
+            if(thisFilm != null)
+            {
+                thisFilm.Quantity++;
+            }
+            else
+            {
+                var newCartItem = db.Films.Where(f => f.FilmId == filmId).SingleOrDefault();
+
+                if(newCartItem != null)
+                {
+                    var cartItem = new CartItem
+                    {
+                        Film = newCartItem,
+                        Quantity = 1,
+                        Value = newCartItem.Price
+                    };
+
+                    cart.Add(cartItem);
+                }
+
+                session.Set(Consts.CartSessionKey, cart);
+            }
+        }
+
+        public int RemoveFromCart(int filmId)
+        {
+            var cart = GetItems();
+            var thisFilm = cart.Find(f => f.Film.FilmId == filmId);
+
+            if(thisFilm != null)
+            {
+                if(thisFilm.Quantity > 1)
+                {
+                    thisFilm.Quantity--;
+                    return thisFilm.Quantity;
+                }
+                else
+                {
+                    cart.Remove(thisFilm);
+                }
+            }
+
+            return 0;
+        }
+
+        public decimal GetCartValue()
+        {
+            var cart = GetItems();
+            return cart.Sum(i => (i.Quantity * i.Value));
+        }
+
+        public int GetCartQuantity()
+        {
+            var cart = GetItems();
+            return cart.Sum(i => i.Quantity);
+        }
+    }
+}
