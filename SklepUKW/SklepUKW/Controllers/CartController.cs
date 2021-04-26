2742f09e4b77f87e1d54b7af95a96230ef505a9f
Adrianna Piszcz <adrianna.piszcz@vp.pl>
2021-04-26 21:15:46 +0200

    Koszyk - implementacja

---
+++ SklepUKW/SklepUKW/Controllers/CartController.cs
@@ -0,0 +1,47 @@
+﻿using SklepUKW.DAL;
+using SklepUKW.Infrastructure;
+using SklepUKW.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SklepUKW.Controllers
+{
+    public class CartController : Controller
+    {
+        CartManager cartManager;
+        FilmsContext db;
+        ISessionManager session;
+
+        public CartController()
+        {
+            db = new FilmsContext();
+            session = new SessionManager();
+            cartManager = new CartManager(db, session);
+        }
+
+        // GET: Cart
+        public ActionResult Index()
+        {
+            var cart = cartManager.GetItems();
+            var totalValue = cartManager.GetCartValue();
+
+            CartViewModel cvm = new CartViewModel()
+            {
+                CartItems = cart,
+                TotalPrice = totalValue
+            };
+
+            return View(cvm);
+        }
+
+        public ActionResult AddToCart(int id)
+        {
+            cartManager.AddToCart(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SklepUKW/SklepUKW/ViewModels/CartViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SklepUKW.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SklepUKW.ViewModels
+{
+    public class CartViewModel
+    {
+        public List<CartItem> CartItems { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+    }
+}
