f6d9c98e0e4b956ceb677776e52cf1f02d28205d
Adrianna Piszcz <adrianna.piszcz@vp.pl>
2021-05-10 17:35:32 +0200

    Mechanizm logowania i rejestracji - backend

---
+++ SklepUKW/SklepUKW/Models/AccountViewModels.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SklepUKW.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Musisz wprowadzić login")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Musisz wprowadzić login")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+        [Display(Name = "Zapamiętaj mnie")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Musisz wprowadzić login")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Musisz wprowadzić login")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Musisz wprowadzić login")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Potwierdź hasło")]
+        [Compare("Password", ErrorMessage = "Hasła muszą być jednakowe!")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ SklepUKW/SklepUKW/Models/UserData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SklepUKW.Models
+{
+    public class UserData
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Adres { get; set; }
+
+        public string Miasto { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telefon { get; set; }
+    }
+}
